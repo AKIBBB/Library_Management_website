a1e46a05210beb3d82c2ba75ad2f4ae06078311a
AKIBBB <aaakibmonir@gmail.com>
2024-12-08 00:47:59 +0600

    first commit

--- Library_Management_website/settings.py
+++ Library_Management_website/settings.py
@@ -126,9 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'  # URL to access media files in development
-MEDIA_ROOT = BASE_DIR / 'media' 
+MEDIA_ROOT = BASE_DIR / 'staticfiles' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
